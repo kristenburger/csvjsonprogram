a132caabf248a3231aa2ccc76b460574fb70d17c
Kristen Burger <kburger@stu.jsu.edu>
2017-10-04 13:45:12 -0500

    Redid error check

--- src/main/java/edu/jsu/mcis/Converter.java
+++ src/main/java/edu/jsu/mcis/Converter.java
@@ -193,7 +193,7 @@ public class Converter {
                 
                 csvdata[i] = csvdata[i].replace("[","");
                 csvdata[i] = csvdata[i].replace("]","");
-			} 
+			}
                 /* Split csvdata[i] into row elements (using comma as delimiter) */
 
                 String[] elements = csvdata[i].split(",");
@@ -221,7 +221,7 @@ public class Converter {
 			 for (int i = 1; i < elements.length; i++){  
 			    
 			   rowdata[i] = elements[i];
-			}
+			
 			   
 				
 				
